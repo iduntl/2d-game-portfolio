51b935d5a232564902a5322c1c2110cd02ddaeaf
Idun Larsen <idun.tokle.larsen@tietoevry.com>
2024-07-29 14:55:26 +0200

    Add dialogue data

--- 2d-game-portfolio/src/constants.js
+++ 2d-game-portfolio/src/constants.js
@@ -1,1 +1,26 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my computer, where I do my programming. 
+        Lately, I've been working mostly on frontend using React, 
+        and some backend with .NET. I also like to use Java and Python, 
+        but programming is fun no matter the language! 
+        Check out my <a href="https://github.com/iduntl" target="_blank">Github</a>. 
+        There isn't a lot there, but take a look at what is.`,
+    "sofa-table": 'This is where I relax and hang out when I have friends over. I love having visitors.',
+    "cs-degree": `This is my bachelor's degree in <a href="https://www.uib.no/en/studies/BASV-KOGNI" target="_blank">
+        Cognitive Sciende</a> from the University of Bergen.
+        It got me a position as a graduate intern for Tietoevry!`,
+    tv: `I like to relax by watching <a href="https://www.youtube.com/watch?v=lwjBR4oHO8E&t=627s" target="_blank">
+        my dads youtube videos</a> when I need to rest.`,
+    bed: "This is where I sleep at night.",
+    resume: `This is my desk, seems like my resume is lying here. Take a look if you want!
+        If you have any questions, or if you just want to get in touch, contact me on
+        <a href="https://www.linkedin.com/in/idun-tokle-larsen-9a4937221/" target="_none">linkedIn</a>, 
+        or you can email me at larsenidun@gmail.com!`,
+    projects: `Check out some of my projects <a href="https://github.com/iduntl" target="_blank">Github</a>. 
+        There isn't a lot there, but take a look at what is.`,
+    library: `My bookshelf! There is both Norwegian and English books here as I am fluent in both. 
+        There is even a French one, but that is just for show.`,
+    exit: "To leave my portfolio, just close the tab. Bye!"
+}
